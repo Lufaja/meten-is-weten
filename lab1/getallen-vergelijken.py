c57c168f4f99d0d985a12f2b4c4ab2255142bb1a
Lukas <Lukas.v.hulst@gmail.com>
2021-09-09 10:08:27 +0200

    Min en Max

--- lab1/getallen-vergelijken.py
+++ lab1/getallen-vergelijken.py
@@ -4,10 +4,14 @@ b = input('b = ')
 
 if a > b:
     Max = a
+    Min = b
     print('a is het grootste getal: ' + str(Max))
 elif a < b:
-    
+    Max = b
     Min = a
     print('a is het kleinste getal: ' + str(Min))
 else:
     print("a en b zijn even groot")
+
+print("Het minimum is: " + str(Min))
+print("Het maximum is: " + str(Max))
